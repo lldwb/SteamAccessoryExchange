a9afe6d6e3e62377641c5593770966dd55d58011
lldwb <3247187440@qq.com>
2023-06-16 08:49:51 +0800

    1234

---
+++ sae-service-api/src/main/java/top/lldwb/sae/api/controller/UserController.java
@@ -0,0 +1,16 @@
+package top.lldwb.sae.api.controller;
+
+import org.nf.web.annotation.RequestMapping;
+import org.nf.web.servlet.View;
+
+/**
+ * @author 安然的尾巴
+ * @version 1.0
+ */
+public class UserController {
+
+    @RequestMapping("/user/getUser")
+    public View getUser(){
+        return null;
+    }
+}
