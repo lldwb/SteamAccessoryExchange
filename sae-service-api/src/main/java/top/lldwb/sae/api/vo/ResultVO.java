2d3cb5695d4bb17044d02683b01817df3c82a332
lldwb <3247187440@qq.com>
2023-06-16 08:49:51 +0800

    1234

---
+++ sae-service-api/src/main/java/top/lldwb/sae/api/vo/ResultVO.java
@@ -0,0 +1,23 @@
+package top.lldwb.sae.api.vo;
+
+import lombok.Data;
+
+/**
+ * @author 安然的尾巴
+ * @version 1.0
+ */
+@Data
+public class ResultVO<T> {
+    /**
+     * 业务状态码
+     */
+    private Integer code;
+    /**
+     * 响应的消息
+     */
+    private String message;
+    /**
+     * 响应的数据
+     */
+    private T data;
+}
