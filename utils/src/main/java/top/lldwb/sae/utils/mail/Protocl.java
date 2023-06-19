94b967db04e28e8b012d9d91f07aa7677799e765
lldwb <3247187440@qq.com>
2023-06-19 12:00:06 +0800

    1

---
+++ utils/src/main/java/top/lldwb/sae/utils/mail/Protocl.java
@@ -0,0 +1,15 @@
+package top.lldwb.sae.utils.mail;
+
+/**
+ * 邮箱传输协议类型
+ * @author 安然的尾巴
+ * @version 1.0
+ */
+public enum Protocl {
+    SMTP("smtp"),
+    POP3("POP3");
+    String value;
+    Protocl(String value){
+        this.value = value;
+    }
+}
