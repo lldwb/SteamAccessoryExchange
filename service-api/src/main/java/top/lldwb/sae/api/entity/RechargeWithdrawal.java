ee690c02d0ece15a506e07b05cd3054e7dceee06
天渊 <117790537+tianyuanlizian@users.noreply.github.com>
2023-06-19 20:42:57 +0800

    充值提现接口完善

--- service-api/src/main/java/top/lldwb/sae/api/entity/RechargeWithdrawal.java
+++
@@ -1,8 +0,0 @@
-package top.lldwb.sae.api.entity;
-
-/**
- * Author: tianyuan
- * Date: 2023/6/19
- */
-public class RechargeWithdrawal {
-}
---
+++ service/src/main/java/top/lldwb/sae/service/model/paymentcenter/RechargeWithdrawal.java
@@ -0,0 +1,65 @@
+package top.lldwb.sae.service.model.paymentcenter;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.nf.db.util.Column;
+
+import java.util.Date;
+
+/**
+ * Author: tianyuan
+ * Date: 2023/6/19
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RechargeWithdrawal {
+    /**
+     * ID
+     */
+    @Column("rw_id")
+    private Integer rwId;
+
+    /**
+     * 支付方式·
+     */
+    @Column("rw_way")
+    private Integer rwWay;
+
+    /**
+     * 支付/提现时间
+     */
+    @Column("rw_time")
+    private Date rwTime;
+
+    /**
+     * 金额
+     */
+    @Column("rw_amount_of_money")
+    private Double rwAmountOfMoney;
+
+    /**
+     * 余额
+     */
+    @Column("rw_balance")
+    private Double rwBalance;
+
+    /**
+     * 类型（充值/提现）
+     */
+    @Column("rw_type")
+    private Integer rwType;
+
+    /**
+     * 用户id
+     */
+    @Column("user_id")
+    private Integer userId;
+
+    /**
+     * 第三方订单号
+     */
+    @Column("third_party_order")
+    private Integer thirdPartyOrder;
+}
