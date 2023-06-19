5bbe7764599837e828f107b2e26a4d88d40b0021
lldwb <3247187440@qq.com>
2023-06-19 15:58:30 +0800

    1

--- utils/src/main/java/top/lldwb/sae/utils/MySqlUtil.java
+++ utils/src/main/java/top/lldwb/sae/utils/MySqlUtil.java
@@ -1,10 +1,13 @@
 package top.lldwb.sae.utils;
 
 import org.nf.db.util.SqlExecutor;
+import org.nf.db.util.result.BeanHandler;
+import org.nf.db.util.result.BeanListHandler;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.List;
 
 /**
  * @author 安然的尾巴
@@ -34,5 +37,43 @@ public class MySqlUtil {
     Connection conn = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
 
     public MySqlUtil() throws SQLException {
+        sqlExecutor = new SqlExecutor(DriverManager.getConnection(URL, USER_NAME, PASSWORD));
+    }
+
+    /**
+     * 从数据库中查询符合条件的记录，并将结果封装为指定类型的List<T>对象
+     *
+     * @param t   用于封装结果的Java对象
+     * @param sql SQL语句
+     * @param obj SQL语句中的参数列表
+     * @param <T> Java类型
+     * @return 返回符合条件的记录封装为的List<T>对象
+     * @throws SQLException
+     */
+    public <T> List<T> queryList(T t, String sql, Object... obj) throws SQLException, NoSuchFieldException, InstantiationException, IllegalAccessException {
+        BeanListHandler<T> handler = new BeanListHandler<T>((Class<T>) t.getClass());
+        List<T> list = sqlExecutor.executeQuery(sql, handler, obj);
+//        DbUtils.close(conn);
+        return list;
+//        return null;
+    }
+
+    /**
+     * 从数据库中查询符合条件的记录，并将结果封装为指定类型的T对象
+     *
+     * @param t   用于封装结果的Java对象
+     * @param sql SQL语句
+     * @param obj SQL语句中的参数列表
+     * @param <T> Java类型
+     * @return 返回符合条件的记录封装为的T对象
+     * @throws SQLException
+     */
+    public <T> T queryT(T t, String sql, Object... obj) throws SQLException, NoSuchFieldException, InstantiationException, IllegalAccessException {
+        BeanHandler<T> handler = new BeanHandler<T>((Class<T>) t.getClass());
+        return sqlExecutor.executeQuery(sql, handler, obj);
+    }
+
+    public int update(String sql, Object... obj) throws SQLException {
+        return sqlExecutor.executeUpdate(sql, obj);
     }
 }
