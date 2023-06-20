fd187a5aea0d78b1edd96889dcccde32e966f04c
天渊 <117790537+tianyuanlizian@users.noreply.github.com>
2023-06-20 19:32:03 +0800

    充值提现功能完善

--- service/src/main/java/top/lldwb/sae/service/service/paymentcenter/RechargeWithdrawalService.java
+++ service/src/main/java/top/lldwb/sae/service/service/paymentcenter/RechargeWithdrawalService.java
@@ -9,9 +9,24 @@ import java.util.List;
  * Date: 2023/6/19
  */
 public interface RechargeWithdrawalService {
+    /**
+     * 充值/提现
+     * @param id 用户id
+     * @param money 余额
+     * @return
+     */
     int recharge(int id ,double money );
 
+    /**
+     * 根据id查询余额
+     * @param id 用户id
+     * @return
+     */
     RechargeWithdrawal getBalance(int id);
 
+    /**
+     * 查询全部信息
+     * @return
+     */
     List<RechargeWithdrawal>  getRechargeWithdrawal();
 }
