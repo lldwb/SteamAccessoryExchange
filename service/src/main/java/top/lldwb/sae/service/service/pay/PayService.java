956d16f8c5788b2a279b494482b161c72390ccf5
qinxin <215168884@qq.com>
2023-06-20 10:04:01 +0800

    sb

--- service/src/main/java/top/lldwb/sae/service/service/pay/PayService.java
+++
@@ -1,8 +0,0 @@
-package top.lldwb.sae.service.service.pay;
-
-/**
- * @author 安然的尾巴
- * @version 1.0
- */
-public interface PayService {
-}
--- service/src/main/java/top/lldwb/sae/service/service/user/UserService.java
+++
@@ -1,8 +0,0 @@
-package top.lldwb.sae.service.service.user;
-
-/**
- * @author 安然的尾巴
- * @version 1.0
- */
-public interface UserService {
-}
