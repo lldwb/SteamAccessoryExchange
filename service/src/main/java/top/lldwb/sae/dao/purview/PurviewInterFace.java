740e2aab586b6adde723d077a55f15de76259feb
ljh <2026319183@qq.com>
2023-06-25 20:21:11 +0800

    完成权限增删改查

---
+++ service/src/main/java/top/lldwb/sae/dao/purview/PurviewInterFace.java
@@ -0,0 +1,51 @@
+package top.lldwb.sae.dao.purview;
+
+import top.lldwb.sae.entity.purview.Purview;
+
+import java.util.List;
+
+/***
+ * @Date(时间)2023-06-25
+ * @Author 家辉
+ *
+ * 权限接口抽象类
+ */
+public interface PurviewInterFace {
+
+    /***
+     * 添加数据
+     * @param purview
+     * @return
+     */
+    int purviewADD(Purview purview) ;
+
+    /***
+     * 根据id修改数据
+     * @param id
+     * @return
+     */
+    int purviewDelete(int id) ;
+
+
+    /***
+     * 修改数据
+     * @param purview
+     * @return
+     */
+    int purviewUpdate(Purview purview) ;
+
+    /***
+     * 查询所有
+     * @return
+     */
+    List<Purview> purviewList() ;
+
+
+    /***
+     * 根据条件ID查询权限数据
+     * @param id
+     * @return
+     */
+    Purview purviewUserIDQuery(int id);
+
+}
