14aeea55bcbd5fdd462ed93357d6ef46c3795db6
lldwb <3247187440@qq.com>
2023-06-28 15:41:11 +0800

    修改过滤器BUG

--- service-api/src/main/java/top/lldwb/sae/api/filterate/PermissionFiltering_Zero.java
+++ service-api/src/main/java/top/lldwb/sae/api/filterate/PermissionFiltering_Zero.java
@@ -17,31 +17,28 @@ import java.io.IOException;
  *
  * 这个过滤器主要针对用户，用于拦截请求并且做权限验证
  */
+
 public class PermissionFiltering_Zero implements Filter {
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        Filter.super.init(filterConfig);
-        System.out.println("过滤中...");
-    }
+//    @Override
+//    public void init(FilterConfig filterConfig) throws ServletException {
+//        Filter.super.init(filterConfig);
+//        System.out.println("过滤中...");
+//    }
 
     @Override
     public void doFilter(ServletRequest servletRequest,
                          ServletResponse servletResponse,
                          FilterChain filterChain) throws IOException, ServletException {
-        //转换一下
-        HttpServletRequest req = (HttpServletRequest) servletRequest;
-        HttpServletResponse resp = (HttpServletResponse) servletRequest;
 
-        //请求获取url地址
-        String url = req.getRequestURI();
+        System.out.println(servletRequest.getParameter(servletRequest.getParameter("userName")));
 
         /***
          * 如果请求的URI以"xx.html"、"xx.do"或"xx.do"结尾,
          * 则表示用户正在登录或执行在线操作，此时通过filterChain.doFilter(request, response)方法直接放行，让请求继续向目标资源进行处理。
          */
         UserServiceInterFace service = new UserService();
-        if (service.selectStatus(req.getServerName()) == 1) {
-            filterChain.doFilter(req, resp);
+        if (service.selectStatus(servletRequest.getParameter("userName")) == 1) {
+            filterChain.doFilter(servletRequest, servletResponse);
         } else {
             throw new AllException(500, "账号被封禁不可以登录");
         }
@@ -49,9 +46,9 @@ public class PermissionFiltering_Zero implements Filter {
 
     }
 
-    @Override
-    public void destroy() {
-        Filter.super.destroy();
-        System.out.println("过滤结束!");
-    }
+//    @Override
+//    public void destroy() {
+//        Filter.super.destroy();
+//        System.out.println("过滤结束!");
+//    }
 }
