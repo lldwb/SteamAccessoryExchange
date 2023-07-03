4b951f05017f6cc5e864a2dfd15c9ca705880c76
qinxin <215168884@qq.com>
2023-07-03 10:59:28 +0800

    完善活动接口

--- service-api/src/main/java/top/lldwb/sae/api/controller/activity/ActivityController.java
+++ service-api/src/main/java/top/lldwb/sae/api/controller/activity/ActivityController.java
@@ -1,15 +1,63 @@
 package top.lldwb.sae.api.controller.activity;
-
-import org.nf.web.annotation.RequestMapping;
-import org.nf.web.annotation.RequestParam;
+import org.nf.web.annotation.Model;
 import org.nf.web.servlet.View;
+
+
 import org.nf.web.servlet.view.JsonView;
-import top.lldwb.sae.dao.activity.ActivityDao;
-import top.lldwb.sae.dao.activity.Impl.ActivityDaoImpl;
+import top.lldwb.sae.entity.activity.Activity;
+import top.lldwb.sae.service.activity.ActivityService;
+import top.lldwb.sae.service.activity.impl.ActivityServiceImpl;
+import top.lldwb.sae.utils.vo.ResultVO;
+
+import java.util.List;
 
 public class ActivityController {
-    public View getActivityController(){
-        ActivityDao dao = new ActivityDaoImpl();
-        return null;
+   private  ActivityService service = new ActivityServiceImpl();
+    /**
+     * 添加数据
+     * */
+    public View getActivity(@Model Activity activity){
+        int insert = service.insert(activity);
+        ResultVO vo = new ResultVO();
+        vo.setCode(200);
+        vo.setData(insert);
+        vo.setMessage("添加成功");
+        return new JsonView(vo);
+    }
+
+    public View getDeleteActivity(Activity ac_id){
+         int delete = service.delete(ac_id);
+        ResultVO vo = new ResultVO();
+        vo.setCode(200);
+        vo.setData(delete);
+        vo.setMessage("删除成功");
+        return new JsonView(vo);
+    }
+
+    public View getUpdateActivity(Activity activity){
+        int update = service.update(activity);
+        ResultVO vo = new ResultVO();
+        vo.setCode(200);
+        vo.setData(update);
+        vo.setMessage("修改成功");
+        return new JsonView(vo);
+    }
+
+    public View getSelectActivity(int ac_id){
+        Activity select = service.select(ac_id);
+        ResultVO vo = new ResultVO();
+        vo.setCode(200);
+        vo.setData(select);
+        vo.setMessage("查询成功");
+        return new JsonView(vo);
+    }
+
+    public View getActivityList(Activity activity){
+        List<Activity> selectlist = service.selectlist(activity);
+        ResultVO vo = new ResultVO();
+        vo.setCode(200);
+        vo.setData(selectlist);
+        vo.setMessage("查询全部成功");
+        return new JsonView(vo);
     }
 }
--- service/src/main/java/top/lldwb/sae/service/activity/impl/ActivityServiceImpl.java
+++ service/src/main/java/top/lldwb/sae/service/activity/impl/ActivityServiceImpl.java
@@ -10,33 +10,27 @@ import java.util.List;
 public class ActivityServiceImpl implements ActivityService {
      ActivityDao  dao = new ActivityDaoImpl();
     @Override
-    public void insert(Activity activity) {
-        int insert = dao.insert(activity);
-        System.out.println(insert);
+    public int insert(Activity activity) {
+        return dao.insert(activity);
     }
 
     @Override
-    public void delete(Activity ac_id) {
-        int delete = dao.delete(ac_id);
-        System.out.println(delete);
+    public int delete(Activity ac_id) {
+      return dao.delete(ac_id);
     }
 
     @Override
-    public void update(Activity activity) {
-        int update = dao.update(activity);
-        System.out.println(update);
-
+    public int  update(Activity activity) {
+        return dao.update(activity);
     }
 
     @Override
     public Activity select(int ac_id) {
-
         return dao.select(ac_id);
     }
 
     @Override
     public List<Activity> selectlist(Activity activity) {
-
         return dao.selectlist(activity);
     }
 }
